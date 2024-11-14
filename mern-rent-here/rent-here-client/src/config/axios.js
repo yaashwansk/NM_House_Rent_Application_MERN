4bdc7274ef81e99b248edd6c22fc7656ce3831bf
yaashwansk <74255139+yaashwansk@users.noreply.github.com>
2024-11-14 23:17:50 +0530

    Delete mern-rent-here directory

--- mern-rent-here/rent-here-client/src/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const instance = axios.create({
-    // baseURL: 'http://localhost:10000',
-    baseURL: 'https://mern-rent-here.onrender.com',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
